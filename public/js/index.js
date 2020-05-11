a4d92608ea8973938bc56d52410b0c557447b5f5
David Garzon <edgarzon93@gmail.com>
2020-05-11 12:02:01 -0500

    extenso

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,332 @@
 function show(){
     var piz = document.getElementById('box_pizza1');
     var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'block'; 
     panz.style.display = 'none'; 
-    piz.style.display = 'block';  
-   }
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none'; 
+    
+}
 function show2(){
+    var piz = document.getElementById('box_pizza1');
     var panz = document.getElementById('box_pizza2');
-    var piz=document.getElementById('box_pizza1')
-    panz.style.display = 'block'; 
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
     piz.style.display = 'none'; 
- }
-
+    panz.style.display = 'block'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none';
+}
+function show3(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='block';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none';
+}
+function show4(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'block';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none';
+}
+  function show5(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'block';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none';
+}
+function show6(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'block';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none';
+}
+function show7(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'block';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none';
+}
+function show8(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'block';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none';
+}
+function show9(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'block';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none';
+}
+function show10(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'block';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'none';
+}
+function show11(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'block';
+    bebidas.style.display = 'none';
+}
+function show12(){
+    var piz = document.getElementById('box_pizza1');
+    var panz = document.getElementById('box_pizza2');
+    var l_s = document.getElementById('box_pizza3');
+    var m_infantil =  document.getElementById('box_pizza4');
+    var hamb =  document.getElementById('box_pizza5');
+    var perro =  document.getElementById('box_pizza6');
+    var sandw =  document.getElementById('box_pizza7');
+    var shawar =  document.getElementById('box_pizza8');
+    var d_rapidas =  document.getElementById('box_pizza9');
+    var p_locas =  document.getElementById('box_pizza10'); 
+    var mazor =  document.getElementById('box_pizza11');
+    var bebidas =  document.getElementById('box_pizza12');
+    piz.style.display = 'none'; 
+    panz.style.display = 'none'; 
+    l_s.style.display ='none';
+    m_infantil.style.display = 'none';
+    hamb.style.display = 'none';
+    perro.style.display = 'none';
+    sandw.style.display = 'none';
+    shawar.style.display = 'none';
+    d_rapidas.style.display = 'none';
+    p_locas.style.display = 'none';
+    mazor.style.display = 'none';
+    bebidas.style.display = 'block';
+   
+}
 var images1 = [
     'css/Imagenes/Pizzas1.JPG',
     'css/Imagenes/Pizzas2.JPG',
     'css/Imagenes/Pizzas3.JPG',
     'css/Imagenes/Pizzas4.JPG'
-];
+];  
+
 var images2 = [
     'css/Imagenes/Panzeroti1.JPG',
     'css/Imagenes/Panzeroti2.JPG'    
 ];
+ 
 var num = 0;
 function next1() {  
+    
     var slider1 = document.getElementById('slider1');
     num++;
     if(num >= images1.length) {
@@ -38,7 +342,8 @@ function prev1() {
     }
     slider1.src = images1[num];
 }
-function next2() {  
+
+/* function next2() {  
     var slider2 = document.getElementById('slider2');
     num++;
     if(num >= images2.length) {
@@ -54,6 +359,6 @@ function prev2() {
     }
     slider2.src = images2[num];
 }
-
+ */
  
   
