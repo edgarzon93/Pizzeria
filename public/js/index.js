46c31e69f42c087578f4e21f5647eaab51a83e62
David Garzon <edgarzon93@gmail.com>
2020-05-12 07:42:15 -0500

    primera parte de refactorizacion de codigo js

--- public/js/index.js
+++ public/js/index.js
@@ -1,79 +1,103 @@
-function show(){
-    var dis = document.getElementById('box_pizza1');
-    dis.style.display = 'block';
-    var piz = document.getElementById('encabezado');
-    piz.src = 'css/Imagenes/Pizza_header.JPG';
-    var piz2 = document.getElementById('slider');
-    piz2.src = 'css/Imagenes/pizza_c.png'; 
-}
-function show2(){
-    var dis = document.getElementById('box_pizza1');
-    dis.style.display = 'block';
-    var piz = document.getElementById('encabezado');
-    piz.src = 'css/Imagenes/Panzeroti_header.JPG';
-    piz.style.height = '110px';
-    var piz2 = document.getElementById('slider');
-    piz2.src = 'css/Imagenes/panzeroti_c.jpg'; 
-    piz2.style.height = '320px';    
-}
-function show3(){
-    var dis = document.getElementById('box_pizza1');
-    dis.style.display = 'block';
-    var piz = document.getElementById('encabezado');
-    piz.src = 'css/Imagenes/Lasagna_spaguettis_header.JPG';
-    piz.style.height = '130px';
-    var piz2 = document.getElementById('slider');
-    piz2.src = 'css/Imagenes/lasagna_c.jpg'; 
-    piz2.style.height = '300px';    
-}
+let menuActual = 'hamburguesas';
 
 var images1 = [
     'css/Imagenes/Pizzas1.JPG',
     'css/Imagenes/Pizzas2.JPG',
     'css/Imagenes/Pizzas3.JPG',
     'css/Imagenes/Pizzas4.JPG'
-];  
+];
 
 var images2 = [
     'css/Imagenes/Panzeroti1.JPG',
-    'css/Imagenes/Panzeroti2.JPG'    
+    'css/Imagenes/Panzeroti2.JPG'
 ];
- 
+
 var num = 0;
-function next1() {  
-    
-    var slider = document.getElementById('slider');
-    num++;
-    if(num >= images1.length) {
-        num = 0;
+
+function show(menu) {
+    switch (menu) {
+        case 'pizza':
+            menuActual = 'pizza'
+            document.getElementById('box_pizza1').style.display = 'block';
+            document.getElementById('encabezado').src = 'css/Imagenes/Pizza_header.JPG';
+            document.getElementById('slider').src = 'css/Imagenes/pizza_c.png';
+            break;
+        case 'panzeroti':
+            menuActual = 'panzeroti'
+            var dis = document.getElementById('box_pizza1');
+            dis.style.display = 'block';
+            var piz = document.getElementById('encabezado');
+            piz.src = 'css/Imagenes/Panzeroti_header.JPG';
+            piz.style.height = '110px';
+            var piz2 = document.getElementById('slider');
+            piz2.src = 'css/Imagenes/panzeroti_c.jpg';
+            piz2.style.height = '320px';
+            break;
     }
-    slider.src = images1[num];
 }
-function prev1() {
-    var slider = document.getElementById('slider');
-    num--;
-    if(num < 0) {
-        num = images1.length-1;
-    }
-    slider.src = images1[num];
+function show2() {
+    menuActual = 'panzeroti'
+    var dis = document.getElementById('box_pizza1');
+    dis.style.display = 'block';
+    var piz = document.getElementById('encabezado');
+    piz.src = 'css/Imagenes/Panzeroti_header.JPG';
+    piz.style.height = '110px';
+    var piz2 = document.getElementById('slider');
+    piz2.src = 'css/Imagenes/panzeroti_c.jpg';
+    piz2.style.height = '320px';
+}
+function show3() {
+    var dis = document.getElementById('box_pizza1');
+    dis.style.display = 'block';
+    var piz = document.getElementById('encabezado');
+    piz.src = 'css/Imagenes/Lasagna_spaguettis_header.JPG';
+    piz.style.height = '130px';
+    var piz2 = document.getElementById('slider');
+    piz2.src = 'css/Imagenes/lasagna_c.jpg';
+    piz2.style.height = '300px';
 }
 
-function next2() {  
-    var slider2 = document.getElementById('slider2');
+
+function next1() {
+
+    let array;
+    switch (menuActual) {
+        case 'pizza':
+            array = images1
+            break;
+        case 'panzeroti':
+            array = images2
+            break;
+    }
+
+    var slider = document.getElementById('slider');
     num++;
-    if(num >= images2.length) {
+    if (num >= array.length) {
         num = 0;
     }
-    slider2.src = images2[num];
+    slider.src = array[num];
 }
-function prev2() {
-    var slider2 = document.getElementById('slider2');
-    num--;
-    if(num < 0) {
-        num = images2.length-1;
+function prev1() {
+
+    let array;
+    switch (menuActual) {
+        case 'pizza':
+            array = images1
+            break;
+        case 'panzeroti':
+            array = images2
+            break;
     }
-    slider2.src = images2[num];
+
+    var slider = document.getElementById('slider');
+    num--;
+    if (num < 0) {
+        num = array.length - 1;
+    }
+    slider.src = array[num];
 }
- 
- 
-  
+
+
+
+
+
